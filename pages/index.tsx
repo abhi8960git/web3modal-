6ca2a159a08305d81eed30793b21b82c996a3b04
Abhishek <21bcs116@nith.ac.in>
2023-09-23 19:30:42 +0530

    set up web3wallet for phone :zap:

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,50 @@
-import { Inter } from 'next/font/google'
-import Web3Modal from 'web3modal';
-import { ethers } from 'ethers';
-import { useState } from 'react';
+import { Inter } from "next/font/google";
+import { ethers } from "ethers";
+import { useState } from "react";
 
-const providerOptions = {}
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Button, Web3Modal } from "@web3modal/react";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { arbitrum, mainnet, polygon } from "wagmi/chains";
 
-const inter = Inter({ subsets: ['latin'] })
+const chains = [arbitrum, mainnet, polygon];
+const projectId = "2fd657a4f9b9da4b73ccec4ae4a7fd34";
+
+const providerOptions = {};
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [walletAddress, setWalletAddress] = useState('');
+  const [walletAddress, setWalletAddress] = useState("");
 
-  async function connectWallet() {
-    try {
-      let web3modal = new Web3Modal({
-        cacheProvider: false,
-        providerOptions
-      });
-      const web3ModalInstance = await web3modal.connect();
-      const web3ModalProvider = new ethers.providers.Web3Provider(web3ModalInstance);
-      const signer = web3ModalProvider.getSigner();
-      const address = await signer.getAddress();
-      console.log(web3ModalInstance, web3ModalProvider, address);
-      setWalletAddress(address);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  const { publicClient } = configureChains(chains, [
+    w3mProvider({ projectId }),
+  ]);
+  const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors: w3mConnectors({ projectId, chains }),
+    publicClient,
+  });
+  const ethereumClient = new EthereumClient(wagmiConfig, chains);
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p>hello</p>
-        <button onClick={connectWallet}>Connect Wallet</button>
-        {walletAddress && <p>Wallet address: {walletAddress}</p>}
-      </div>
+   
+
+      <WagmiConfig config={wagmiConfig}>
+      <p>Web3 Modal</p>
+      <Web3Button/>
+      </WagmiConfig>
+
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+  
+
     </main>
-  )
+  );
 }
