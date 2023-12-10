9a20555862f328e6e0315a7a6d962e775afbb478
Abhishek <21bcs116@nith.ac.in>
2023-12-10 12:25:00 +0530

    added logic to connect web3wallet and connect smart contracts :zap:

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,38 @@
-import { Inter } from "next/font/google";
-import { ethers } from "ethers";
-import { useState } from "react";
 
-import {
-  EthereumClient,
-  w3mConnectors,
-  w3mProvider,
-} from "@web3modal/ethereum";
-import { Web3Button, Web3Modal } from "@web3modal/react";
-import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { arbitrum, mainnet, polygon } from "wagmi/chains";
+import { useWeb3ModalProvider, useWeb3ModalAccount } from '@web3modal/ethers5/react'
+import { ethers } from 'ethers'
+import React, { useEffect } from "react";
+import Web3Wallet from "@/context/Web3Modal";
+import abi from '../services/abi.json'
+import { Container } from 'postcss';
+const address:string ='0x4dA194bC069bDf5a5ee580632dAF0b986b45287f';
+export default function RootLayout() {
 
-const chains = [arbitrum, mainnet, polygon];
-const projectId = "2fd657a4f9b9da4b73ccec4ae4a7fd34";
+  const { address, chainId, isConnected } = useWeb3ModalAccount()
+  const { walletProvider } = useWeb3ModalProvider()
+  async function getBalance(){
+    if(!isConnected) {
+      alert("Please connect Metamask")
+    }
+    const ethersProvider =  new ethers.providers.Web3Provider(walletProvider)
+    const signer = await ethersProvider.getSigner()
+    // The Contract object
+    const contract = new ethers.Contract(address, abi, signer)
+   return contract;
+  }
 
-const providerOptions = {};
 
-const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  const [walletAddress, setWalletAddress] = useState("");
-
-  const { publicClient } = configureChains(chains, [
-    w3mProvider({ projectId }),
-  ]);
-  const wagmiConfig = createConfig({
-    autoConnect: true,
-    connectors: w3mConnectors({ projectId, chains }),
-    publicClient,
-  });
-  const ethereumClient = new EthereumClient(wagmiConfig, chains);
+  useEffect(()=>{
+   getBalance();
+  },[])
 
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-   
-
-      <WagmiConfig config={wagmiConfig}>
-      <p>Web3 Modal</p>
-      <Web3Button/>
-      </WagmiConfig>
-
-      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
   
+     <div>
+     <Web3Wallet/>
 
-    </main>
+     </div>
+ 
   );
 }
